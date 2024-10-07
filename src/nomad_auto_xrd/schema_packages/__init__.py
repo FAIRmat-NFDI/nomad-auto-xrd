6d352cb9e5ebba4e91f09a41bf8728cb07269804
Pepe Marquez <pepemarquezprieto@gmail.com>
2024-10-07 23:56:05 +0200

    Added first draft of schema for analysis

--- src/nomad_auto_xrd/schema_packages/__init__.py
+++ src/nomad_auto_xrd/schema_packages/__init__.py
@@ -15,3 +15,18 @@ auto_xrd = NewSchemaPackageEntryPoint(
     name='auto_xrd',
     description='New schema package entry point configuration.',
 )
+
+
+class XRDAnalaysisSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_auto_xrd.schema_packages.auto_xrd_analysis import m_package
+
+        return m_package
+
+
+auto_xrd_analysis = XRDAnalaysisSchemaPackageEntryPoint(
+    name='auto_xrd_analysis',
+    description='New schema package entry point configuration.',
+)
