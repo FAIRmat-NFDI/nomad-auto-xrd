52299663c8df916124be920f52a22c876fdfac62
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-02-25 15:26:54 +0100

    Inference schemas

---
+++ src/nomad_auto_xrd/schema_packages/inference.py
@@ -0,0 +1,69 @@
+from nomad.datamodel.metainfo.annotations import ELNAnnotation
+from nomad.datamodel.metainfo.basesections import SectionReference
+from nomad.metainfo import Quantity, SubSection
+from nomad_analysis.general.schema import AnalysisResult
+from nomad_analysis.jupyter.schema import ELNJupyterAnalysis
+from nomad_measurements.xrd.schema import ELNXRayDiffraction
+
+from nomad_auto_xrd.schema_packages.auto_xrd import AutoXRDModel
+
+
+class AutoXRDAnalysisInput(SectionReference):
+    """
+    Base class for all `AutoXRDAnalysis` inputs.
+    """
+
+
+class XRDMeasurement(AutoXRDAnalysisInput):
+    reference = Quantity(
+        type=ELNXRayDiffraction,
+        description='A reference to an `ELNXRayDiffraction` entry.',
+        a_eln=ELNAnnotation(
+            component='ReferenceEditQuantity',
+        ),
+    )
+
+
+class AutoXRDModelReference(AutoXRDAnalysisInput):
+    reference = Quantity(
+        type=AutoXRDModel,
+        description='A reference to an `AutoXRDModel` entry.',
+        a_eln=ELNAnnotation(
+            component='ReferenceEditQuantity',
+        ),
+    )
+
+
+class IdentifiedPhase(AnalysisResult):
+    """
+    Section for the identified phase.
+    """
+
+    phase = Quantity(
+        type=str,
+        description='The identified phase.',
+    )
+    reference_cif = Quantity(
+        type=str,
+        description='The reference CIF file.',
+        a_eln=ELNAnnotation(
+            component='FileEditQuantity',
+        ),
+    )
+    probability = Quantity(
+        type=float,
+        description='The probability that the phase is present.',
+    )
+
+
+class AutoXRDAnalysis(ELNJupyterAnalysis):
+    inputs = SubSection(
+        section_def=AutoXRDAnalysisInput,
+        description='The input section for the auto XRD analysis.',
+        repeats=True,
+    )
+    outputs = SubSection(
+        section_def=AnalysisResult,
+        description='The output section for the auto XRD analysis.',
+        repeats=True,
+    )
