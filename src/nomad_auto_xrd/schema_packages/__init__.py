750f29e93a58c855c0312986b796f96dec3a8e5f
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-02-26 12:18:17 +0100

    Schema package init

--- src/nomad_auto_xrd/schema_packages/__init__.py
+++ src/nomad_auto_xrd/schema_packages/__init__.py
@@ -30,3 +30,28 @@ auto_xrd_analysis = XRDAnalaysisSchemaPackageEntryPoint(
     name='auto_xrd_analysis',
     description='New schema package entry point configuration.',
 )
+
+
+class InferenceSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_auto_xrd.schema_packages.inference import m_package
+
+        return m_package
+
+
+class TrainingSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_auto_xrd.schema_packages.training import m_package
+
+        return m_package
+
+
+inference_schema = InferenceSchemaPackageEntryPoint(
+    name='Auto XRD Inference Schema',
+    description='Schema for performing Auto XRD analysis.',
+)
+
+training_schema = TrainingSchemaPackageEntryPoint(
+    name='Auto XRD Training Schema',
+    description='Schema for training Auto XRD models.',
+)
