8a155d3e31b76a1db50897a7a5a526c2787a457a
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-02-28 12:56:23 +0100

    Add test for loading schemas

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -2,6 +2,12 @@ import os.path
 
 from nomad.client import normalize_all, parse
 
+from nomad_auto_xrd.schema_packages import (
+    analysis_schema,
+    model_schema,
+    training_schema,
+)
+
 
 def test_schema_package():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
@@ -9,3 +15,9 @@ def test_schema_package():
     normalize_all(entry_archive)
 
     assert entry_archive.data is not None
+
+
+def test_loading_schema_packages():
+    model_schema.load()
+    training_schema.load()
+    analysis_schema.load()
