28c8f34b1479a5c7214d9b263b59c4a2b6436203
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-04-10 21:00:11 +0200

    Testing analysis: use archive yaml inputs for entries

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,16 +1,11 @@
 import os
 
 import pytest
-from nomad.client import normalize_all
-from nomad.datamodel import EntryArchive
+from nomad.client import normalize_all, parse
 
 from nomad_auto_xrd.analysis import analyse
-from nomad_auto_xrd.schema import (
-    AutoXRDAnalysis,
-    AutoXRDModel,
-)
 
-data_dir = os.path.join('tests', 'data', 'analysis')
+data_dir = os.path.abspath(os.path.join('tests', 'data', 'analysis'))
 
 xrd_files = [
     os.path.join(data_dir, 'XRD-918-16_10.xrdml'),
@@ -19,6 +14,7 @@ xrd_files = [
 log_levels = ['error', 'critical']
 
 
+# TODO separate into analysis entry setup fixture and analysis functionality
 @pytest.mark.parametrize(
     'parsed_measurement_archives, caplog',
     [(xrd_files, log_levels)],
@@ -40,24 +36,21 @@ def test_analysis(parsed_measurement_archives, caplog):
         for path in os.listdir(os.path.join(data_dir, 'References'))
         if path.endswith('.cif')
     ]
-    model = AutoXRDModel(includes_pdf=True)
-    model.reference_files = reference_files
-    model.xrd_model = os.path.join(data_dir, 'Models', 'XRD_Model.h5')
-    model.pdf_model = os.path.join(data_dir, 'Models', 'PDF_Model.h5')
+    model = parse(os.path.join(data_dir, 'AutoXRDModel.archive.yaml'))[0]
+    model.data.reference_files = reference_files
+    model.data.xrd_model = os.path.join(data_dir, 'Models', 'XRD_Model.h5')
+    model.data.pdf_model = os.path.join(data_dir, 'Models', 'PDF_Model.h5')
 
-    normalize_all(EntryArchive(data=model))
+    normalize_all(model)
 
     # prepare the analysis
-    analysis = AutoXRDAnalysis()
-    analysis.m_setdefault('analysis_settings')
-    analysis.analysis_settings.auto_xrd_model = model
-    analysis.analysis_settings.min_angle = 10
-    analysis.analysis_settings.max_angle = 60
+    analysis = parse(os.path.join(data_dir, 'AutoXRDAnalysis.archive.yaml'))[0]
+    analysis.analysis_settings.auto_xrd_model = model.data
     analysis.m_setdefault('inputs/0')
     analysis.inputs[0].reference = parsed_measurement_archives[0].data
     analysis.m_setdefault('inputs/1')
     analysis.inputs[1].reference = parsed_measurement_archives[1].data
-    normalize_all(EntryArchive(data=analysis))
+    normalize_all(analysis)
 
     results = analyse(analysis)
     print(results)
