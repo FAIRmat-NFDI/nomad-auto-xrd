738ed797a5846e8779fd298630981dee7f6b105d
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-08-02 17:19:30 +0200

    Ruff

--- src/nomad_auto_xrd/analysis.py
+++ src/nomad_auto_xrd/analysis.py
@@ -20,7 +20,6 @@ import os
 import shutil
 import tempfile
 import time
-from collections import defaultdict
 from collections.abc import Callable
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Optional
