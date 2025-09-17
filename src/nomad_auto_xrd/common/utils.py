7c939651641d6e3bdf46a6349e67ee45344ab6d2
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-09-17 17:42:50 +0200

    Combine preprocessors and move to utils

--- src/nomad_auto_xrd/common/utils.py
+++ src/nomad_auto_xrd/common/utils.py
@@ -15,8 +15,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+from typing import TYPE_CHECKING
+
 from nomad import infrastructure
 from nomad.processing.data import Upload
+from nomad_analysis.utils import get_reference
+
+from nomad_auto_xrd.common.models import AnalysisInput
+
+if TYPE_CHECKING:
+    from structlog.stdlib import BoundLogger
 
 
 def get_upload(upload_id: str, user_id: str) -> Upload | None:
@@ -39,3 +48,55 @@ def get_upload(upload_id: str, user_id: str) -> Upload | None:
         )
 
     return upload
+
+def pattern_preprocessor(
+    xrd_entry_archive: dict, logger: 'BoundLogger | None' = None
+) -> list[AnalysisInput] | None:
+    """
+    Extract relevant data from `XRayDiffraction` sections into `AnalysisInput` objects.
+
+    Args:
+        xrd_entry_archive (dict): The XRD entry archive to process.
+        logger (BoundLogger | None): Optional logger for logging warnings.
+
+    Returns:
+        list[AnalysisInput]: List of processed data ready for analysis.
+    """
+    if 'data_files' in xrd_entry_archive['data']:
+        filenames = xrd_entry_archive['data']['data_files']
+    elif 'data_file' in xrd_entry_archive['data']:
+        filenames = [xrd_entry_archive['data']['data_file']]
+    else:
+        raise AttributeError('No data file(s) found in the XRD section.')
+    patterns = xrd_entry_archive['results']['properties']['structural'][
+        'diffraction_pattern'
+    ]
+    if len(filenames) != len(patterns):
+        raise ValueError(
+            'Number of data files does not match number of diffraction patterns.'
+        )
+    if not patterns:
+        raise ValueError('No diffraction patterns found in the XRD section.')
+    analysis_inputs = []
+    for idx, (filename, pattern) in enumerate(zip(filenames, patterns)):
+        two_theta = pattern['two_theta_angles']
+        intensity = pattern['intensity']
+        if two_theta is None or intensity is None:
+            (logger.warning if logger else print)(
+                f'Warning: Missing two_theta or intensity in pattern {idx} '
+                f'of entry {xrd_entry_archive["metadata"]["entry_id"]}. Skipping.'
+            )
+            continue
+        analysis_inputs.append(
+            AnalysisInput(
+                filename=os.path.basename(filename),
+                two_theta=two_theta,
+                intensity=intensity,
+                measurement_m_proxy=get_reference(
+                    xrd_entry_archive['metadata']['upload_id'],
+                    xrd_entry_archive['metadata']['entry_id'],
+                    f'data/results/{idx}',
+                ),
+            )
+        )
+    return analysis_inputs
