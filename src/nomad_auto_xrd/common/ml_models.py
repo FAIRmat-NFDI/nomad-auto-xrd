5b2a0e44b025067dcdaef46534185c7e88a34241
Sarthak Kapoor <sarthakkapoor@hotmail.com>
2025-12-16 16:09:18 +0100

    Refactor imports; init optimizer with LR; modify `build_model` signature

--- src/nomad_auto_xrd/common/ml_models.py
+++ src/nomad_auto_xrd/common/ml_models.py
@@ -1,7 +1,17 @@
-import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import (
+    BatchNormalization,
+    Conv1D,
+    Dense,
+    Flatten,
+    Layer,
+    MaxPooling1D,
+)
+from tensorflow.keras.optimizers import Adam
+from tensorflow.nn import dropout
 
 
-class CustomDropout(tf.keras.layers.Layer):
+class CustomDropout(Layer):
     def __init__(self, rate, **kwargs):
         super().__init__(**kwargs)
         self.rate = rate
@@ -13,10 +23,15 @@ class CustomDropout(tf.keras.layers.Layer):
 
     # Always apply dropout
     def call(self, inputs, training=None):
-        return tf.nn.dropout(inputs, rate=self.rate)
+        return dropout(inputs, rate=self.rate)
 
 
-def build_model(input_shape, n_phases, is_pdf, n_dense=[3100, 1200], dropout_rate=0.7):
+def build_model(
+    input_shape,
+    n_phases,
+    is_pdf,
+    learning_rate=0.001,
+):
     """
     Builds the CNN model based on whether it is for PDF or XRD analysis.
     """
@@ -25,57 +40,60 @@ def build_model(input_shape, n_phases, is_pdf, n_dense=[3100, 1200], dropout_rat
         # Architecture for PDF analysis
         layers.extend(
             [
-                tf.keras.layers.Conv1D(
+                Conv1D(
                     64, 60, activation='relu', padding='same', input_shape=input_shape
                 ),
-                tf.keras.layers.MaxPooling1D(3, strides=2, padding='same'),
-                tf.keras.layers.MaxPooling1D(3, strides=2, padding='same'),
-                tf.keras.layers.MaxPooling1D(2, strides=2, padding='same'),
-                tf.keras.layers.MaxPooling1D(1, strides=2, padding='same'),
-                tf.keras.layers.MaxPooling1D(1, strides=2, padding='same'),
-                tf.keras.layers.MaxPooling1D(1, strides=2, padding='same'),
+                MaxPooling1D(3, strides=2, padding='same'),
+                MaxPooling1D(3, strides=2, padding='same'),
+                MaxPooling1D(2, strides=2, padding='same'),
+                MaxPooling1D(1, strides=2, padding='same'),
+                MaxPooling1D(1, strides=2, padding='same'),
+                MaxPooling1D(1, strides=2, padding='same'),
             ]
         )
     else:
         # Architecture for XRD analysis
         layers.extend(
             [
-                tf.keras.layers.Conv1D(
+                Conv1D(
                     64, 35, activation='relu', padding='same', input_shape=input_shape
                 ),
-                tf.keras.layers.MaxPooling1D(3, strides=2, padding='same'),
-                tf.keras.layers.Conv1D(64, 30, activation='relu', padding='same'),
-                tf.keras.layers.MaxPooling1D(3, strides=2, padding='same'),
-                tf.keras.layers.Conv1D(64, 25, activation='relu', padding='same'),
-                tf.keras.layers.MaxPooling1D(2, strides=2, padding='same'),
-                tf.keras.layers.Conv1D(64, 20, activation='relu', padding='same'),
-                tf.keras.layers.MaxPooling1D(1, strides=2, padding='same'),
-                tf.keras.layers.Conv1D(64, 15, activation='relu', padding='same'),
-                tf.keras.layers.MaxPooling1D(1, strides=2, padding='same'),
-                tf.keras.layers.Conv1D(64, 10, activation='relu', padding='same'),
-                tf.keras.layers.MaxPooling1D(1, strides=2, padding='same'),
+                MaxPooling1D(3, strides=2, padding='same'),
+                Conv1D(64, 30, activation='relu', padding='same'),
+                MaxPooling1D(3, strides=2, padding='same'),
+                Conv1D(64, 25, activation='relu', padding='same'),
+                MaxPooling1D(2, strides=2, padding='same'),
+                Conv1D(64, 20, activation='relu', padding='same'),
+                MaxPooling1D(1, strides=2, padding='same'),
+                Conv1D(64, 15, activation='relu', padding='same'),
+                MaxPooling1D(1, strides=2, padding='same'),
+                Conv1D(64, 10, activation='relu', padding='same'),
+                MaxPooling1D(1, strides=2, padding='same'),
             ]
         )
 
     # Common layers
+    n_dense = [3100, 1200]
+    dropout_rate = 0.7
     layers.extend(
         [
-            tf.keras.layers.Flatten(),
+            Flatten(),
             CustomDropout(dropout_rate),
-            tf.keras.layers.Dense(n_dense[0], activation='relu'),
-            tf.keras.layers.BatchNormalization(),
+            Dense(n_dense[0], activation='relu'),
+            BatchNormalization(),
             CustomDropout(dropout_rate),
-            tf.keras.layers.Dense(n_dense[1], activation='relu'),
-            tf.keras.layers.BatchNormalization(),
+            Dense(n_dense[1], activation='relu'),
+            BatchNormalization(),
             CustomDropout(dropout_rate),
-            tf.keras.layers.Dense(n_phases, activation='softmax'),
+            Dense(n_phases, activation='softmax'),
         ]
     )
+    optimizer = Adam(learning_rate=learning_rate)
 
-    model = tf.keras.Sequential(layers)
+    model = Sequential(layers)
     model.compile(
         loss='categorical_crossentropy',
-        optimizer='adam',
+        optimizer=optimizer,
         metrics=['categorical_accuracy'],
     )
     return model
